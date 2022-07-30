a01234a710e49e6f8729e892492f1f5041384645
Your Name <you@example.com>
2022-07-30 16:25:09 -0500

    evaluate models

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
-from utils.Camera import Camera
+from utils.Camera_Color import Camera
 from utils.MaskRCNN import MaskRCNN
 from utils.IdentificationModel import IdentificationModel
 from utils.metrics.measuring import tracing_start, tracing_mem
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     source_1 = 'Datasets/Propio/cameras/pasillo/cam_1/soft/Pasillo_001_sf.mp4'
     source_2 = 'Datasets/Propio/cameras/pasillo/cam_2/soft/Pasillo_001_sf.mp4'
 
-    output_path = "outputs/multi-input/video_2.avi"
+    output_path = "results/color-RGB/model 2/video_2.avi"
 
     true_class_name = 'Luis'
 
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     (h, w) = (None, None)
 
     # for o1, o2 in zip(Camera(src=source_1, id=0).read_video(model.segment, id_model, 'outputs/run_1/cam1_detections.csv'), Camera(src=source_2, id=1).read_video(model.segment, id_model, 'outputs/run_1/cam2_detections.csv')):
-    for o in Camera(src=source_2, id=1).read_video(model.segment, id_model, 'outputs/multi-input/cam2_detections.csv'):
+    for o in Camera(src=source_2, id=1).read_video(model.segment, id_model, 'results/color-RGB/model 2/cam2_detections.csv'):
         # o = np.concatenate((o1, o2), axis=1)
         if writer is None:
             (h, w) = o.shape[:2]
